8f332660ad991e13fadf3ac471ba0d342f7e1581
pjoshirev <pareshjoshi@revana.com>
2017-01-19 09:50:18 -0700

    Update app.py
    changed if else

--- app.py
+++ app.py
@@ -29,18 +29,20 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    
+    if req.get("result").get("action") == "yahooWeatherForecast":  
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = makeYqlQuery(req)
+        if yql_query is None:
+            return {}
+        yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+        result = urllib.urlopen(yql_url).read()
+        data = json.loads(result)
+        res = makeWebhookResult(data)
+        return res
+    else: 
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
+  
 
 def makeYqlQuery(req):
     result = req.get("result")
